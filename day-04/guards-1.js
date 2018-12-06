d4719cc675b2501912f5d3cde0a0710f01a07d5c
Julian Nicholls <juliannicholls29@gmail.com>
2018-12-06 10:59:36 +0000

    Day 4 part 1 complete

--- day-04/guards-1.js
+++ day-04/guards-1.js
@@ -3,35 +3,93 @@ const fs = require('fs');
 function loadTimeline() {
   const text = fs.readFileSync('./input.txt', 'utf8');
   const lines = text.split(/\n/).filter(line => line.length > 0);
-  const entries = {};
+  const entries = [];
 
   lines.forEach(line => {
-    const [_, date, hour, minute] = line.match(
-      /^\[1518-(\d\d-\d\d) (\d\d):(\d\d)/
+    const [_, date, time, event] = line.match(
+      /^\[1518-(\d\d-\d\d) (\d\d:\d\d)\] (.*)$/
     );
-    const event = line.substr(19);
 
-    if (hour === '00') {
-      if (entries[date])
-        entries[date].push({ minute: parseInt(minute, 10), event });
-      else entries[date] = [{ minute: parseInt(minute, 10), event }];
+    entries.push(`${date} ${time} ${event}`);
+  });
+
+  return entries.sort();
+}
+
+function collectGuardTime(entries) {
+  let num = 0;
+  const guards = {};
+
+  entries.forEach(entry => {
+    const guardMatch = entry.match(/Guard #(\d{1,4})/);
+
+    if (guardMatch) num = parseInt(guardMatch[1]);
+    else {
+      const sleepMatch = entry.match(/00:(\d{2}) .*(sleep|wake)/);
+      if (!sleepMatch) console.log({ entry, sleepMatch });
+      const event = {
+        minute: parseInt(sleepMatch[1], 10),
+        type: sleepMatch[2]
+      };
+
+      if (!guards[num]) guards[num] = [event];
+      else guards[num].push(event);
     }
   });
 
-  for (key in entries) {
-    entries[key] = entries[key].sort((a, b) => a.minute - b.minute);
+  return guards;
+}
+
+function flattenGuards(guards) {
+  const guardSleeps = {};
+
+  for (let num in guards) {
+    guardSleeps[num] = new Array(60).fill(0);
+
+    for (let idx = 0; idx < guards[num].length; idx += 2) {
+      const start = guards[num][idx].minute;
+      const end = guards[num][idx + 1].minute;
+
+      for (let min = start; min < end; ++min) {
+        ++guardSleeps[num][min];
+      }
+    }
   }
 
-  return entries;
+  return guardSleeps;
+}
+
+function totalGuards(times) {
+  const totals = {};
+  let maxSleep = { num: 0, value: 0 };
+
+  for (let num in times) {
+    totals[num] = times[num].reduce((acc, min) => acc + min);
+
+    if (totals[num] > maxSleep.value) {
+      maxSleep = { num, value: totals[num] };
+    }
+  }
+
+  const highestFrequency = Math.max(...times[maxSleep.num]);
+  const minute = times[maxSleep.num].indexOf(highestFrequency);
+
+  return {
+    totals,
+    maxSleep,
+    highestFrequency,
+    minute
+  };
 }
 
 const entries = loadTimeline();
-
 console.log('Entries:', entries.length);
-console.log('An entry:', entries['11-08']);
 
-for (key in entries) {
-  //  if (!entries[key][0].event.test(/Guard #\d{1,4}/)) {
-  console.log(key, entries[key][0]);
-  //  }
-}
+const guards = collectGuardTime(entries);
+console.log('Guards:', Object.keys(guards).length);
+
+const times = flattenGuards(guards);
+console.log('Times:', Object.keys(times).length);
+
+const result = totalGuards(times);
+console.log(result);
