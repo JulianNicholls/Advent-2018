d590e62c2f591016fc9b0ebccbde518e1b57d8a4
Julian Nicholls <juliannicholls29@gmail.com>
2018-12-06 11:49:09 +0000

    Day 5 complete

---
+++ day-05/alchemy-2.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const polymer = fs.readFileSync('./input.txt', 'utf8');
+
+function purge(polymer, purgeUnit) {
+  let newPolymer = '';
+
+  polymer.split('').forEach(unit => {
+    if (unit.toUpperCase() !== purgeUnit) newPolymer += unit;
+  });
+
+  return newPolymer;
+}
+
+function react(polymer) {
+  let reacted = false;
+  let newPolymer = '';
+
+  for (let i = 0; i < polymer.length; ++i) {
+    if (
+      i < polymer.length - 1 &&
+      polymer[i].toLowerCase() === polymer[i + 1].toLowerCase() &&
+      polymer[i] !== polymer[i + 1]
+    ) {
+      ++i;
+      reacted = true;
+    } else if (polymer[i] !== '\n') newPolymer += polymer[i];
+  }
+
+  return { polymer: newPolymer, reacted };
+}
+
+for (i = 0; i < 26; ++i) {
+  const unit = String.fromCharCode(i + 65);
+  const polymerToTest = purge(polymer, unit);
+  let result = react(polymerToTest);
+
+  while (result.reacted) {
+    result = react(result.polymer);
+  }
+
+  console.log(`Unit: ${unit}, length: ${result.polymer.length}`);
+}
